3f8bc115e79530301dae95795e63d6183373db39
22voisbo <49483178+22voisbo@users.noreply.github.com>
2019-11-06 08:26:13 -0500

    Add files via upload

---
+++ test_scores.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# display a welcome message
+print("The Test Scores program")
+print()
+print("Enter 4 test scores")
+print("======================")
+
+# get scores from the user
+total_score = 0       # initialize the variable for accumulating scores
+total_score += int(input("Enter test score: "))
+total_score += int(input("Enter test score: "))
+total_score += int(input("Enter test score: "))
+total_score += int(input("Enter test score: "))
+
+# calculate average score
+average_score = round(total_score /)
+             
+# format and display the result
+print("======================")
+print("Total Score:  ", total_score,
+      "\nAverage Score:", average_score)
+print()
+print("Bye")
+
+
